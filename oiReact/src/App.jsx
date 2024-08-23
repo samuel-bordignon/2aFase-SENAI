2441171b26fadbbdf117fe98c2ed2e3fe2c84c1e
samuelBordignon <samuelborwie@gmail.com>
2024-08-23 17:15:45 -0300

    Criação do primeiro projeto com React bonit~]ao

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>Bebês👶👶</h1>
+        <h2>Mortos</h2>
+        <h2>Degolados</h2>
+        <h2>E muito mais</h2>
+        <p>Ultimamente, em nossas rotinas, se deparar com corpinhos nas ruas está cada vez mais comum :(</p>
+    </>
+  )
+}
+
+export default App
