42ad999e2d180d377b31f0ff72e0a136a9fee6fd
samuelBordignon <samuelborwie@gmail.com>
2024-08-29 17:21:03 -0300

    criação de componentes e mais umas coisas legais e gostosinhas

---
+++ componentes/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Atalhos VS code</h1>
+      <h2>Coisas legais pra tua vida</h2>
+      <p>
+      Comando para o emmet da ul: ul>li*6
+      </p>
+
+      <ul>
+        <li>Atalho: alta + z para quebra automatica de linha.</li>
+        <li>Atalho: alt + clique esquerdo para adicinar vários cursores</li>
+        <li>Atalho: windows + . abre o teclado de emojis</li>
+        <li>Atalho: ctrl + ; para comentar o código selecionado</li>
+        <li>Atalho: alt + (⬆⬇) para mover a linha/bloco inteiro</li>
+        <li>Atalho: alt + (⬆⬇) para copia a linha/bloco inteiro</li>
+        <li>Atalho: shift + j ou ctrl + ' abre/fecha o terminal</li>
+      </ul>
+
+      <p>Extensões VS code:</p>
+      <ul>
+        <li>ES7 React/Redux/GraphQL...</li>
+        <li>Auto Rename Tag</li>
+        <li>Color Highlight</li>
+        <li>Power Mode</li>
+        <li></li>
+      </ul>
+
+    </>
+  )
+}
+
+export default App
---
+++ componentesDeVerdade/src/components/Links.jsx
@@ -0,0 +1,10 @@
+function Links() {
+  return (
+    <div>
+      <p>Link 1 | Link 2 | Link 3   (tudo de verdade!)</p>
+    </div>
+  )
+}
+
+export default Links
+
