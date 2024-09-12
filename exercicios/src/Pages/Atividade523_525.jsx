34faa89674e211107049198daeb39540ca32daf5
SamuelBordignon <samuelborwie@gmail.com>
2024-09-12 20:27:47 -0300

    teste do notebbok novo

--- exercicios/src/Pages/Atividade523_525.jsx
+++ exercicios/src/Pages/Atividade523_525.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ValorVenda from '../Components/523-525/ValorVenda'
 
 function Atividade523_525() {
   return (
     <div>
-      
+      <ValorVenda />
     </div>
   )
 }
